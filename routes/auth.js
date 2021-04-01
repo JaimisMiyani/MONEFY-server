3ce6545c165dccc03e3de7603d5193a2e6639518
You-Know-Who-V <rajgondalia123@gmail.com>
2021-04-01 19:15:13 +0530

    add bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 const { registerValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
@@ -23,18 +23,23 @@ router.post('/register', async (req, res) => {
         return;
     }
 
+    // hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
 
     // save user
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user.id });
     } catch (error) {
         res.status(400).send(error);
     }
